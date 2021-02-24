d1fbd133a17490e61ab96253fbddbbc444ab68e9
0xAT7 <33043331+0xAT7@users.noreply.github.com>
2021-02-24 04:12:03 +0200

    Update grapTLD.py

--- grapTLD.py
+++ grapTLD.py
@@ -4,9 +4,9 @@ filepath = sys.argv[1]
 
 with open(filepath, 'r') as file:
     for line in file:
-        if "." in line:
+        if ".com" in line or ".net" in line or ".org" in line or ".uk" in line or ".fr" in line or ".edu" in line or ".de" in line or ".eu" in line or ".ie" in line or ".int" in line or ".mil" in line or ".biz" in line or ".io" in line or ".jp" in line or ".services" in line or ".nl" in line:
             x = line[:line.rfind("/")]
-            if "http" in x:    
+            if "http" in x:
                 y = x.split('://')[1]
                 if "www" in y:
                     z = y.split('www.')[1]
