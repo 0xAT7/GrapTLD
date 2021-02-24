9b063201dad1f5e7b9c21c6e70f8c53e75ed4fc5
0xAT7 <33043331+0xAT7@users.noreply.github.com>
2021-02-24 20:47:12 +0200

    Update grapTLD.py

--- grapTLD.py
+++ grapTLD.py
@@ -2,22 +2,33 @@ import sys
 
 filepath = sys.argv[1]
 
-with open(filepath, 'r') as file:
-    for line in file:
-        tld = [".com", ".net", ".org", ".uk", ".fr", ".edu", ".de", ".eu", ".ie", ".int", ".mil", ".biz", ".io", ".jp", ".services", ".nl"]
-        for tl in tld:
-            if tl in line: 
-                x = line[:line.rfind("/")]
-                if "http" in x:
-                    y = x.split('://')[1]
-                    if "www" in y:
-                        z = y.split('www.')[1]
-                        print(z , end='\n')
+def gettld():
+    with open(filepath, 'r') as file:
+        for line in file:
+            tld = [".com", ".net", ".org", ".uk", ".fr", ".edu", ".de", ".eu", ".ie", ".int", ".mil", ".biz", ".io", ".jp", ".services", ".nl"]
+            for tl in tld:
+                if tl in line: 
+                    x = line[:line.rfind("/")]
+                    if "http" in x:
+                        y = x.split('://')[1]
+                        if "www" in y:
+                            z = y.split('www.')[1]
+                            print(z , end='\n', file=open("output.txt", "a"))
+                        else:
+                            print(y, file=open("output.txt", "a"))
+                    elif "www" in x:
+                        print(x.split('www.')[1], file=open("output.txt", "a"))
                     else:
-                        print(y)
-                elif "www" in x:
-                    print(x.split('www.')[1])
+                        print(x, file=open("output.txt", "a"))
                 else:
-                    print(x)
-            else:
-                pass
+                    pass
+
+def sorting():
+    with open('output.txt') as result:
+            uniqlines = set(result.readlines())
+            with open('output.txt', 'w') as rmdup:
+                rmdup.writelines(set(uniqlines))
+                print("Done!")
+
+gettld()
+sorting()
