d6f465ab033c0cd6ba33a6bb2de793cba429bb31
0xAT7 <33043331+0xAT7@users.noreply.github.com>
2021-02-24 03:45:54 +0200

    Update grapTLD.py

--- grapTLD.py
+++ grapTLD.py
@@ -1,18 +1,22 @@
+import os
 import sys
 
 filepath = sys.argv[1]
 
 with open(filepath, 'r') as file:
-    for line in file:        
-        x = line[:line.rfind("/")]
-        if "http" in x:    
-            y = x.split('://')[1]
-            if "www" in y:
-                z = y.split('www.')[1]
-                print(z , end='\n')
+    for line in file:
+        if "." in line:
+            x = line[:line.rfind("/")]
+            if "http" in x:    
+                y = x.split('://')[1]
+                if "www" in y:
+                    z = y.split('www.')[1]
+                    print(z , end='\n')
+                else:
+                    print(y)
+            elif "www" in x:
+                print(x.split('www.')[1])
             else:
-                print(y)
-        elif "www" in x:
-            print(x.split('www.')[1])
+                print(x)
         else:
-            print(x)
+            pass
