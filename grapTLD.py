8a253e4f09860f19ec7b3d72ebf711095b0cd928
0xAT7 <33043331+0xAT7@users.noreply.github.com>
2021-02-26 16:18:04 +0200

    Update grapTLD.py

--- grapTLD.py
+++ grapTLD.py
@@ -1,34 +1,50 @@
 import sys
 
-filepath = sys.argv[1]
-
-def gettld():
-    with open(filepath, 'r') as file:
-        for line in file:
-            tld = [".com", ".net", ".org", ".uk", ".fr", ".edu", ".de", ".eu", ".ie", ".int", ".mil", ".biz", ".io", ".jp", ".services", ".nl"]
-            for tl in tld:
-                if tl in line: 
-                    x = line[:line.rfind("/")]
-                    if "http" in x:
-                        y = x.split('://')[1]
-                        if "www" in y:
-                            z = y.split('www.')[1]
-                            print(z , end='\n', file=open("output.txt", "a"))
+try:
+    def gettld(filepath, output):
+        with open(filepath, 'r') as file:
+            for line in file:
+                tld = [".com", ".net", ".org", ".uk", ".fr", ".edu", ".de", ".eu", ".ie", ".int", ".mil", ".biz", ".io", ".jp", ".services", ".nl"]
+                for tl in tld:
+                    if tl and tl + "/" in line:
+                        x = line[:line.rfind("/")]
+                        if "http" in x:
+                            y = x.split('/')[2]
+                            if "www." in y:
+                                z = y.split('www.')[1]
+                                print(z , end='\n', file=open(output, "a"))
+                            else:
+                                print(y, file=open(output, "a"))
+                        elif "www." in x:
+                            print(x.split('www.')[1], file=open("output", "a"))
                         else:
-                            print(y, file=open("output.txt", "a"))
-                    elif "www" in x:
-                        print(x.split('www.')[1], file=open("output.txt", "a"))
+                            x = line.split('/')[0]
+                            print(x, file=open(output, "a"))
+                    elif tl in line:
+                        if "http" in line:
+                            a = line.split('://')[1]
+                            if "www." in a:
+                                b = a.split('www.')[1]
+                                print(b , end='\n', file=open(output, "a"))
+                            else:
+                                print(a , end='\n', file=open(output, "a"))
+                        elif "www." in line:
+                            print(line.split('www.')[1], file=open(output, "a"))
+                        else:
+                            print(line, file=open(output, "a"))
                     else:
-                        print(x, file=open("output.txt", "a"))
-                else:
-                    pass
+                        pass
 
-def sorting():
-    with open('output.txt') as result:
-            uniqlines = set(result.readlines())
-            with open('output.txt', 'w') as rmdup:
-                rmdup.writelines(set(uniqlines))
-                print("Done!")
+    def sorting(output):
+        with open(output) as result:
+                uniqlines = set(result.readlines())
+                lines = filter(lambda x: not x.isspace(), uniqlines)
+                with open(output, 'w') as final_file:
+                    final_file.writelines(set(lines))
 
-gettld()
-sorting()
+    gettld(sys.argv[1] , sys.argv[2])
+    sorting(sys.argv[2])
+    print("Done! Check your file: " + sys.argv[2])
+    
+except:
+    print("Usage: python3 grapTLD.py list.txt output.txt")
